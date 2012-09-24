afe4a130d02ece735971eed8760147995e9018a5
aarmijo <alberto.arm@gmail.com>
2012-09-24 15:33:27 +0200

    EPES demo data

--- distro/src/setup/files/demo/h2.data.sql
+++ distro/src/setup/files/demo/h2.data.sql
@@ -1,28 +1,25 @@
 insert into ACT_ID_GROUP values ('admin',       1, 'System administrator', 'security-role');
-insert into ACT_ID_GROUP values ('user',        1, 'User', 'security-role');
-insert into ACT_ID_GROUP values ('manager',     1, 'Manager', 'security-role');
-insert into ACT_ID_GROUP values ('management',  1, 'Management',  'assignment');
-insert into ACT_ID_GROUP values ('accountancy', 1, 'Accountancy', 'assignment');
-insert into ACT_ID_GROUP values ('engineering', 1, 'Engineering', 'assignment');
-insert into ACT_ID_GROUP values ('sales',       1, 'Sales', 'assignment');
+insert into ACT_ID_GROUP values ('Managers',     1, 'Windmill Manager', 'assignment');
+insert into ACT_ID_GROUP values ('Software engineers', 1, 'Software Engineer', 'assignment');
+insert into ACT_ID_GROUP values ('Mechanics',       1, 'Windmill Mechanic', 'assignment');
 
-insert into ACT_ID_USER values ('kermit', 1, 'Kermit', 'the Frog', 'kermit@localhost', 'kermit', '');
-insert into ACT_ID_MEMBERSHIP values ('kermit', 'admin');
-insert into ACT_ID_MEMBERSHIP values ('kermit', 'manager');
-insert into ACT_ID_MEMBERSHIP values ('kermit', 'management');
-insert into ACT_ID_MEMBERSHIP values ('kermit', 'accountancy');
-insert into ACT_ID_MEMBERSHIP values ('kermit', 'engineering');
-insert into ACT_ID_MEMBERSHIP values ('kermit', 'sales');
+insert into ACT_ID_USER values ('admin', 1, 'Admin', 'the EPES VCN administrator', 'admin@epes', 'admin', null);
+insert into ACT_ID_MEMBERSHIP values ('admin', 'admin');
+insert into ACT_ID_MEMBERSHIP values ('admin', 'Managers');
+insert into ACT_ID_MEMBERSHIP values ('admin', 'Software engineers');
+insert into ACT_ID_MEMBERSHIP values ('admin', 'Mechanics');
 
-insert into ACT_ID_USER values ('fozzie', 1, 'Fozzie', 'Bear', 'fozzie@localhost', 'fozzie', '');
-insert into ACT_ID_MEMBERSHIP values ('fozzie', 'user');
-insert into ACT_ID_MEMBERSHIP values ('fozzie', 'accountancy');
+insert into ACT_ID_USER values ('peter.parker', 1, 'Peter Parker', 'the software engineer', 'peter.parker@epes', 'peter.parker', null);
+insert into ACT_ID_MEMBERSHIP values ('peter.parker', 'Software engineers');
+insert into ACT_ID_MEMBERSHIP values ('peter.parker', 'admin');
 
-insert into ACT_ID_USER values ('gonzo', 1, 'Gonzo', 'the Great', 'gonzo@localhost', 'gonzo', '');
-insert into ACT_ID_MEMBERSHIP values ('gonzo', 'manager');
-insert into ACT_ID_MEMBERSHIP values ('gonzo', 'management');
-insert into ACT_ID_MEMBERSHIP values ('gonzo', 'accountancy');
-insert into ACT_ID_MEMBERSHIP values ('gonzo', 'sales');
+
+insert into ACT_ID_USER values ('john.smith', 1, 'John Smith', 'the manager', 'john.smith@epes', 'john.smith', null);
+insert into ACT_ID_MEMBERSHIP values ('john.smith', 'Managers');
+insert into ACT_ID_MEMBERSHIP values ('john.smith', 'admin');
+
+insert into ACT_ID_USER values ('alice.jones', 1, 'Alice Jones', 'the windmill mechanic', 'alice.jones@epes', 'alice.jones', null);
+insert into ACT_ID_MEMBERSHIP values ('alice.jones', 'Mechanics');
 
 update ACT_GE_PROPERTY
 set VALUE_ = '10'
